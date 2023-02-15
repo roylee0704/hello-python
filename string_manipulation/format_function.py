6040df8693568fa18afe74f07bab2c10e937d864
roylee0704 <roylee0704@gmail.com>
2023-02-16 04:16:17 +0700

    initial commit

---
+++ string_manipulation/format_function.py
@@ -0,0 +1,5 @@
+name = 'Roy'
+age = 31
+message = 'Hello, my name is {name} and I am {age} years old.'.format(
+    name=name, age=age)
+print(message)
---
+++ string_manipulation/formatted_string_literal.py
@@ -0,0 +1,5 @@
+name = 'Roy'
+age = 31
+message = f'Hello, my name is {name} and I am {age} years old.'
+
+print(message)
